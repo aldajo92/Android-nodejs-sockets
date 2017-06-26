03b9ca53efe1824b38783ae88f87b2c17a1d2f28
ADJ Gomez <alejodiscovery20@gmail.com>
2017-06-26 14:40:26 -0500

    improved code

--- server/index.js
+++ server/index.js
@@ -6,59 +6,17 @@ var app = express();
 var SerialPort = require('serialport');
 var ip = require('ip');
 
-const options = commandLineArgs({ name: 'showports', alias: 'p', type: Boolean, description: 'Display serial ports.' })
+const options = commandLineArgs(
+  [
+    { name: 'showports', alias: 's', type: Boolean, description: 'Display serial ports.' },
+    { name: 'port', alias: 'p', type: String, description: 'connect serial ports.'}
+  ],
+  { 
+      partial: true 
+  }
+)
 
-const port = 8080;
-const hostname = ip.address();
-const serialPortName = '/dev/ttyS0';
-const serialPortBaudRate = 9600;
-
-function initSerialPort(){
-    var serialPort1 = new SerialPort(serialPortName, {
-        baudRate: serialPortBaudRate,
-        autoOpen: false
-    });
-
-    serialPort1.open(function (err) {
-        if (err) {
-          return console.log('Error opening port: ', err.message);
-        }
-    });
-
-    serialPort1.on('open', function() {
-        console.log(`${ serialPort1.path } connected!!!`)
-    });
-
-    serialPort1.on('data', function (data) {
-        console.log('Data: ' + data);
-    });
-}
-
-function initServer() {
-    const server = app.listen(port, hostname, () => { console.log(`Listening http://${hostname}:${port}`) });
-    const io = require('socket.io').listen(server);
-
-    app.use(express.static('public'));
-
-    app.get('/hello', function(req, res) {  
-      res.status(200).send("Hello World!");
-    });
-
-    app.get('/', (req, res) => { res.send('Hello world!') });
-    app.post('/', (req, res) => {});
-
-    io.on('connection', function(socket) {  
-        console.log(`Socket ${socket.id} connected...`);
-
-        socket.on('web-message', function(data) {
-            //messages.push(data);
-            io.sockets.emit('android-message', data);
-            console.log(data);
-        });
-
-    });
-    
-}
+console.log(options)
 
 if( options.showports ){
 
@@ -68,7 +26,62 @@ if( options.showports ){
         });
     });
 
-} else {
+}
+
+if ( options.port ){
+
+    const port = 8080;
+    const hostname = ip.address();
+    const serialPortName = options.port;
+    const serialPortBaudRate = 9600;
+
+    function initSerialPort(){
+        var serialPort1 = new SerialPort(serialPortName, {
+            baudRate: serialPortBaudRate,
+            autoOpen: false
+        });
+
+        serialPort1.open(function (err) {
+            if (err) {
+              console.log('Error opening port: ', err.message);
+              process.exit();
+            }
+        });
+
+        serialPort1.on('open', function() {
+            console.log(`${ serialPort1.path } connected!!!`)
+        });
+
+        serialPort1.on('data', function (data) {
+            console.log('Data: ' + data);
+        });
+    }
+
+    function initServer() {
+        const server = app.listen(port, hostname, () => { console.log(`Listening http://${hostname}:${port}`) });
+        const io = require('socket.io').listen(server);
+
+        app.use(express.static('public'));
+
+        app.get('/hello', function(req, res) {  
+          res.status(200).send("Hello World!");
+        });
+
+        app.get('/', (req, res) => { res.send('Hello world!') });
+        app.post('/', (req, res) => {});
+
+        io.on('connection', function(socket) {  
+            console.log(`Socket ${socket.id} connected...`);
+
+            socket.on('web-message', function(data) {
+                //messages.push(data);
+                io.sockets.emit('android-message', data);
+                console.log(data);
+            });
+
+        });
+        
+    }
 
     initSerialPort();
     initServer();
