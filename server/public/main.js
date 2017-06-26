50ec41061dc3ec1c833c6e38dcf3e7c2a3e7eadd
ADJ Gomez <alejodiscovery20@gmail.com>
2017-06-26 16:50:19 -0500

    made some changes

--- server/public/main.js
+++ server/public/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://192.168.1.15:8090', { 'forceNew': true });
+var socket = io.connect('http://192.168.1.14:8090', { 'forceNew': true });
 
 socket.on('messages', function(data) {  
   console.log(data);
