432d270e4af8cec8c54640b4d4f7c49c1f95c2d7
Alejandro Gomez <alejodiscovery20@gmail.com>
2018-09-21 03:29:51 -0500

    improved client

--- server/index.js
+++ server/index.js
@@ -6,17 +6,19 @@ var ip = require('ip');
 var events = require('events');
 var fs = require("fs");
 
+var createInterface = require('readline').createInterface;
+
 const options = commandLineArgs(
-  [
-    { name: 'showports', alias: 's', type: Boolean, description: 'Display serial ports.' },
-    { name: 'port', alias: 'p', type: String, description: 'Connect to an specific serial port.' }
-  ],
-  { partial: true }
+    [
+        { name: 'showports', alias: 's', type: Boolean, description: 'Display serial ports.' },
+        { name: 'port', alias: 'p', type: String, description: 'Connect to an specific serial port.' }
+    ],
+    { partial: true }
 )
 
-if( options.showports ){
+if (options.showports) {
     SerialPort.list(function (err, ports) {
-        ports.forEach(function(port) {
+        ports.forEach(function (port) {
             console.log(port.comName);
         });
     });
@@ -26,22 +28,26 @@ if( options.showports ){
     const serialPortBaudRate = loadConfig.baudrate;
     var eventEmitter = new events.EventEmitter();
 
-    var errorport = function(err){
+    var errorport = function (err) {
         if (err) {
-                console.log('Error: ', err.message);
-                process.exit();
+            console.log('Error: ', err.message);
+            process.exit();
         }
     }
 
-    function initSerialPort(portname){
+    function initSerialPort(portname) {
         var serialPort = new SerialPort(portname, { baudRate: loadConfig.baudrate }, errorport);
 
-        serialPort.on('open', function() {
-            console.log(`${ serialPort.path } connected!!!`)
+        var lineReader = createInterface({
+            input: serialPort
         });
 
-        serialPort.on('data', function (data) {
-            eventEmitter.emit('serialport-data', serialPort.path, data.toString());
+        serialPort.on('open', function () {
+            console.log(`${serialPort.path} connected!!!`)
+        });
+
+        lineReader.on('line', function (line) {
+            eventEmitter.emit('serialport-data', serialPort.path, line.toString());
         });
 
         return serialPort;
@@ -56,58 +62,40 @@ if( options.showports ){
 
         app.use(express.static('public'));
 
-        app.get('/hello', function(req, res) {  
+        app.get('/hello', function (req, res) {
             res.status(200).send("Hello World!");
         });
 
-        app.get('/', (req, res) => { res.send('Hello world!') });
-        app.post('/', (req, res) => {});
+        app.post('/', (req, res) => { });
 
-        io.on('connection', function(socket) {
+        io.on('connection', function (socket) {
             console.log(`Socket ${socket.id} connected...`);
 
-            socket.on('web-message', function(data) {
-                io.sockets.emit('android-message', data);
+            socket.on('web-message', function (data) {
+                io.sockets.emit('web-message', data + " comida");
                 console.log(data);
             });
 
         });
 
-        eventEmitter.on('serialport-data', function (portname, data){
-            var formatted = data.replace(/      /g, "");
-
-            formatted = formatted.replace(/ kg /g,":");
-            values = formatted.split("\r\n");
-
-            var gross_data = values[0].split(":")[0]
-            var tare_data = values[1].split(":")[0]
-            var net_data = values[2].split(":")[0]
-
-            var datamodel = {
-                portname: portname,
-                gross: gross_data,
-                tare: tare_data,
-                net: net_data
-            };
-
-            io.sockets.emit('android-message', datamodel);
-            console.log(datamodel);
+        eventEmitter.on('serialport-data', function (portname, data) {
+            io.sockets.emit('web-message', portname+": "+data);
+            // console.log(data);
         });
     }
 
-    function initFromConfigSerialPorts(){
-        loadConfig.ports.map(function(port){
+    function initFromConfigSerialPorts() {
+        loadConfig.ports.map(function (port) {
             return initSerialPort(port);
         });
     }
 
 
-    if ( options.port ){
+    if (options.port) {
         initSerialPort(options.port);
-    } else {   
+    } else {
         initFromConfigSerialPorts();
     }
 
     initServer();
-
 }
--- server/public/main.js
+++ server/public/main.js
@@ -1,24 +1,7 @@
-var socket = io.connect('http://192.168.1.14:8090', { 'forceNew': true });
-
-socket.on('messages', function(data) {  
-  console.log(data);
-  render(data);
-})
-
-function render (data) {  
-  var html = data.map(function(elem, index) {
-    return(`<div>
-              <strong>${elem.author}</strong>:
-              <em>${elem.text}</em>
-            </div>`);
-  }).join(" ");
-
-  document.getElementById('messages').innerHTML = html;
-}
-
-function addMessage(e) {  
-  var message = document.getElementById('texto').value;
-
-  socket.emit('web-message', message);
-  return false;
-}
+$(function () {
+  var socket = io();
+  // var socket = io.connect('http://192.168.1.14:8090', { 'forceNew': true });
+  socket.on('web-message', function (msg) {
+      $('#messages').append($('<li>').text(msg));
+  });
+});
